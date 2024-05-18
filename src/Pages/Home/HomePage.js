52eda6ebce8f0f5fd36941f2aa8fa70560586d38
amd200 <72322072+amd200@users.noreply.github.com>
2024-05-19 01:39:35 +0300

    Update HomePage.js

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Services from "../../Components/Home/Services";
 import Landing from "../../Components/Home/Landing";
 import Properties from "../../Components/Home/Properties ";
@@ -6,8 +6,9 @@ import Properties from "../../Components/Home/Properties ";
 function HomePage() {
   return (
     <main className="home-page">
+      <iframe src="https://bassthalk.com/" width={"100%"} height={"1000"}></iframe>
       <Landing />
-      <Properties/>
+      <Properties />
       <Services />
     </main>
   );
