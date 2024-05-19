9a25a4b79a4fb190b248699adf26d68fc882d40e
amd200 <72322072+amd200@users.noreply.github.com>
2024-05-20 02:10:14 +0300

    Update UserWishlistPage.jsx

--- src/Pages/User/UserWishlistPage.jsx
+++ src/Pages/User/UserWishlistPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-imporimport React, { useState, useEffect } from 'react';
 import MenuProfile from '../../Components/User/MenuProfile';
 import Card from '../../Components/Uitily/Card';
 
