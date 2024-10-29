3cba34746bb64af784f8cdffd23ab0895d06ef03
amd200 <72322072+amd200@users.noreply.github.com>
2024-10-30 01:22:49 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,18 +9,17 @@ import "react-toastify/dist/ReactToastify.css";
 import NotFoundPage from "./Pages/NotFound/NotFoundPage";
 import DetailsPropertiesPage from "./Pages/DetailsProperties/DetailsPropertiesPage";
 import React from "react";
-import DashboardPage from "./Pages/User/DashboardPage";
 import "react-notifications/lib/notifications.css";
 import LoginPage from "./Pages/Auth/LoginPage";
 import SignUpPage from "./Pages/Auth/SignUpPage";
 import UserProfilePage from "./Pages/User/UserProfilePage";
-import LandLordDashboardPage from "./Pages/Landlord/LandLordDashboardPage";
-import LandLordAddPropertiesPage from "./Pages/Landlord/LandLordAddPropertiesPage";
-import LandLordProfilePage from "./Pages/Landlord/LandLordProfilePage";
 import UserRequsetsPage from "./Pages/User/UserRequsetsPage";
 import LandLordBookingsPage from "./Pages/Landlord/LandLordBookingsPage";
-import LandLordAddListingPage from "./Pages/Landlord/LandLordAddListingPage";
 import UserWishlistPage from "./Pages/User/UserWishlistPage";
+import UserDashboardPage from "./Pages/User/UserDashboardPage";
+import UserAddListingPage from "./Pages/User/UserAddListingPage";
+import UserBookingsPage from "./Pages/User/UserBookingsPage";
+
 function App() {
   return (
     <BrowserRouter>
@@ -33,23 +32,26 @@ function App() {
         <Route path="/signup" element={<SignUpPage />}></Route>
         <Route path="/properties" element={<PropertiesPage />}></Route>
         <Route  path="/bookingproperty"  element={<BookingPropertyPage />}></Route>
-        <Route  path="/detailsproperties" element={<DetailsPropertiesPage />} ></Route>
+        <Route  path="/detailsproperties/:id" element={<DetailsPropertiesPage />} ></Route>
         <Route path="*" element={<NotFoundPage />}></Route>
         {/* End Public Pages */}
 
         {/* Start User Pages */}
-        <Route path="/user/dashboard" element={<DashboardPage />}></Route>
         <Route path="/user/profile" element={<UserProfilePage />}></Route>
         <Route path="/user/requests" element={<UserRequsetsPage />}></Route>
         <Route path="/user/wishlist" element={<UserWishlistPage />}></Route>
+        <Route path="/user/dashboard" element={<UserDashboardPage />} ></Route>
+        <Route path="/user/addlisting" element={<UserAddListingPage />}></Route>
+        <Route path="/user/bookings" element={<UserBookingsPage />} ></Route>
+
         {/* End User Pages */}
 
         {/* Start Admin Pages */}
-        <Route path="/landlord" element={<LandLordDashboardPage />} ></Route>
-        <Route path="/landlord/addlisting" element={<LandLordAddListingPage />}></Route>
-        <Route path="/landlord/addproperty" element={<LandLordAddPropertiesPage />} ></Route>
-        <Route path="/landlord/bookings" element={<LandLordBookingsPage />} ></Route>
-        <Route  path="/landlord/profile" element={<LandLordProfilePage />}></Route>
+        {/* <Route path="/landlord/dashboard" element={<LandLordDashboardPage />} ></Route> */}
+        {/* <Route path="/landlord/addlisting" element={<LandLordAddListingPage />}></Route> */}
+        {/* <Route path="/landlord/addproperty" element={<LandLordAddPropertiesPage />} ></Route> */}
+        {/* <Route path="/landlord/bookings" element={<LandLordBookingsPage />} ></Route> */}
+        {/* <Route  path="/landlord/profile" element={<LandLordProfilePage />}></Route> */}
         {/* End Admin Pages */}
 
       </Routes>
